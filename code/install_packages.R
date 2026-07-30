8fbbae9b147bc85fff8016edc3147406604fca5b
kzhang529 <40783533+kzhang529@users.noreply.github.com>
2026-07-30 14:00:51 -0400

    add R script for installing all required packages

---
+++ code/install_packages.R
@@ -0,0 +1,34 @@
+# install_packages.R
+# Run this script once to install all required packages.
+
+
+# --- Helper: install only if not already installed ---
+install_if_missing <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg, dependencies = TRUE)
+  } else {
+    message(paste0("'", pkg, "' is already installed. Skipping."))
+  }
+}
+
+## #install glmerGOF package (github link: https://github.com/BarkleyBG/glmerGOF)
+remotes::install_github("BarkleyBG/glmerGOF")
+
+
+# --- Required packages ---
+packages <- c("rstudioapi", "lme4", "plyr", "igraph", "numDeriv", "gtools", 
+              "doParallel", "foreach", "knitr", "kableExtra", "ggplot2", "forcats", 
+              "geepack", "tidyverse", "cobalt", "scales", "readxl", 
+              "purrr", "ergm", "sna", "network", "coda", "dplyr", "conflicted",
+              "nortest", "survival", "cubature")
+
+
+# --- Install ---
+invisible(lapply(packages, install_if_missing))
+
+message("Done! All packages are installed.")
+
+
+
+
+
